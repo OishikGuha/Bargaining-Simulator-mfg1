6412af9c0dccbced1b315b197932f8e89bbbdd45
Roboticol <kaushikguha68@gmail.com>
2021-06-28 13:50:14 +0530

    Added player follow

---
+++ Bargaining Simulator/Assets/PlayerFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+
+    public Vector3 offset;
+        
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, 0f, player.position.z) - offset;
+    }
+}
