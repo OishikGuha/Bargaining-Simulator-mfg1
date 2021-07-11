457b6228e9ecb1e60f11627aa33774165c804e75
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2021-07-11 16:55:38 +1000

    Stuff idk what stuff it's almost the deadline

--- Bargaining Simulator/Assets/Scripts/BargainManager.cs
+++ Bargaining Simulator/Assets/Scripts/BargainManager.cs
@@ -5,19 +5,54 @@ using UnityEngine.UI;
 
 public class BargainManager : MonoBehaviour
 {
-
-    public float originalCost;
-    public Vector3 mousePos;
+    public float boundsX;
+    public float boundsY;
+    public GameObject neuronPrefab;
+    public PoolStick poolStick;
+    
+    internal float originalCost;
+    private float newCost;
+    private int originalItems = 0;
+    private int originalNeuronCount = 0;
+    private Player player;
 
     // Start is called before the first frame update
     void Start()
     {
-        // playersCost = originalCost;
+        player = FindObjectOfType<Player>(); // Probably bad code but the jam ends in an hour
     }
 
     void Update()
     {
+        if (player.items.Count > originalItems)
+        {
+            originalItems = player.items.Count;
+            
+            foreach (var item in player.items)
+                originalCost += item.cost;
+        
+            newCost = originalCost;
 
+            int numNeuronsToSpawn;
+            if (originalCost > 100)
+                numNeuronsToSpawn = (int) originalCost / 100 + 10;
+            else if (originalCost > 10)
+                numNeuronsToSpawn = (int) originalCost / 10 + 5;
+            else
+                numNeuronsToSpawn = (int) originalCost;
+            numNeuronsToSpawn -= originalNeuronCount;
+
+            for (int i = 0; i < numNeuronsToSpawn; ++i)
+            {
+                float x = Random.Range(-boundsX, boundsX);
+                float y = Random.Range(-boundsX, boundsY);
+                Transform newTransform = transform;
+                newTransform.parent = transform;
+                newTransform.position += (Vector3) new Vector2(x, y);
+                GameObject neuronObject = Instantiate(neuronPrefab, transform);
+                neuronObject.GetComponent<NeuronClickNotifier>().poolStick = poolStick;
+            }
+        }
     }
 }
 
