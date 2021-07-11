23f454d7c67158246de28d8d0b28c4ca43aeedb8
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2021-07-11 11:12:46 +1000

    Fix pool stick force indicator

--- Bargaining Simulator/Assets/Scripts/PoolStick.cs
+++ Bargaining Simulator/Assets/Scripts/PoolStick.cs
@@ -8,6 +8,8 @@ public class PoolStick : MonoBehaviour
     public SpriteRenderer texture;
     public Vector2 pushForce;
     public float pushBackMaxLength = 1f;
+    public float pushBackMinLength = 0.2f;
+    public float pushBackMaxShowLength = 0.2f;
     
     private Rigidbody2D currentRigidbody = null;
     private Vector2 pushBegin;
@@ -31,12 +33,32 @@ public class PoolStick : MonoBehaviour
             float angle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButton(0) && !isPushing)
             {
                 isPushing = true;
+                pushBegin = mousePosition;
+            }
+            else if (Input.GetMouseButton(0) && isPushing)
+            {
                 Vector2 shootDirectionClamped = Vector2.ClampMagnitude((Vector2) mousePosition - currentRigidbody.position, pushBackMaxLength);
-                transform.position = shootDirection * (-1.25f * (shootDirectionClamped + Vector2.one)) + currentRigidbody.position;
+                
+                Vector2 distanceMultiplierVector = shootDirectionClamped;
+                if (distanceMultiplierVector.x > 0)
+                    distanceMultiplierVector.x += 1f;
+                else
+                    distanceMultiplierVector.x -= 1f;
+                if (distanceMultiplierVector.y > 0)
+                    distanceMultiplierVector.y += 1f;
+                else
+                    distanceMultiplierVector.y -= 1f;
+
+                float distanceMultiplier = -Mathf.Lerp(0f, pushBackMaxShowLength, shootDirectionClamped.magnitude);
+                
+                transform.position = shootDirection * (-1.25f + distanceMultiplier) + currentRigidbody.position;
                 Debug.Log(shootDirectionClamped);
+                Debug.Log(shootDirectionClamped.magnitude);
+                Debug.Log(distanceMultiplier);
+                Debug.Log(Mathf.Lerp(0f, 2f, 0.5f));
             }
             else if (!Input.GetMouseButton(0) && isPushing)
             {
